0d320d6606c9b8a6f66984977e6d503b055c7933
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2026-01-30 19:45:45 -0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/codecrafters-io/sqlite-starter-go/dbobjects"
+	"github.com/xwb1989/sqlparser"
 )
 
 // Usage: your_program.sh sample.db <command>
@@ -26,7 +27,7 @@ func main() {
 	case ".tables":
 		err = handleTables(databaseFilePath)
 	default:
-		log.Fatalf("unknown command %s", command)
+		err = handleQuery(databaseFilePath, command)
 	}
 
 	if err != nil {
@@ -62,7 +63,67 @@ func handleTables(path string) error {
 	return nil
 }
 
+func handleQuery(path, query string) error {
+	stmt, err := sqlparser.Parse(query)
+	if err != nil {
+		return fmt.Errorf("parse query: %w", err)
+	}
+
+	switch stmt := stmt.(type) {
+	case *sqlparser.Select:
+		var tableName string
+		for _, expr := range stmt.From {
+			ate, ok := expr.(*sqlparser.AliasedTableExpr)
+			if !ok {
+				continue
+			}
+
+			tbl, ok := ate.Expr.(sqlparser.TableName)
+			if !ok {
+				continue
+			}
+
+			tableName = tbl.Name.String()
+		}
+		err = getRowCount(path, tableName)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	return fmt.Errorf("unsupported query type: %T", stmt)
+}
+
+func getRowCount(path string, tableName string) error {
+	_, schemaPage, err := loadSchemaPage(path)
+	if err != nil {
+		return err
+	}
+
+	rootPageNum, err := rootPageLookup(tableName, schemaPage)
+	if err != nil {
+		return err
+	}
+
+	_, rootPage, err := loadPage(path, rootPageNum)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(rootPage.CellCount)
+	return nil
+}
+
 func loadSchemaPage(path string) (*dbobjects.DatabaseHeader, *dbobjects.Page, error) {
+	return loadPage(path, 1)
+}
+
+func loadPage(path string, pageNum uint32) (*dbobjects.DatabaseHeader, *dbobjects.Page, error) {
+	if pageNum == 0 {
+		return nil, nil, errors.New("page numbers start at 1")
+	}
+
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, nil, fmt.Errorf("open database: %w", err)
@@ -75,7 +136,7 @@ func loadSchemaPage(path string) (*dbobjects.DatabaseHeader, *dbobjects.Page, er
 		return nil, nil, fmt.Errorf("read database header: %w", err)
 	}
 
-	page, err := dbFile.NewPage(header, 1)
+	page, err := dbFile.NewPage(header, pageNum)
 	if err != nil {
 		return nil, nil, fmt.Errorf("read schema page: %w", err)
 	}
@@ -105,4 +166,35 @@ func extractTableNames(schemaPage *dbobjects.Page) ([]string, error) {
 	}
 
 	return names, nil
+}
+
+func rootPageLookup(tableName string, schemaPage *dbobjects.Page) (uint32, error) {
+	rows, err := dbobjects.ReadAllRows(schemaPage)
+	if err != nil {
+		return 0, fmt.Errorf("read schema rows: %w", err)
+	}
+
+	tblNameIdx := dbobjects.SqliteSchemaCol("tbl_name")
+	rootPageIdx := dbobjects.SqliteSchemaCol("rootpage")
+
+	for _, row := range rows {
+		if tblNameIdx >= len(row.Columns) {
+			return 0, errors.New("tbl_name column missing in schema row")
+		}
+
+		name, ok := row.Columns[tblNameIdx].DecodedValue.(string)
+		if !ok {
+			return 0, fmt.Errorf("rowid %d: tbl_name is not text", row.RowID)
+		}
+
+		if name == tableName {
+			rootPage, ok := row.Columns[rootPageIdx].DecodedValue.(int64)
+			if !ok {
+				return 0, fmt.Errorf("rowid %d: rootpage is not int64", row.RowID)
+			}
+			return uint32(rootPage), nil
+		}
+	}
+
+	return 0, fmt.Errorf("table %s not found in schema", tableName)
 }
